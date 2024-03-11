87a0ec08e81090fec6b7c48d581b6351a4f98bd9
Dimitri JACQUEMONT <jacquemont.dim@gmail.com>
2024-03-11 23:27:08 +0100

    feat: first iteration (ISSUE SENSORS)

--- avoi_dance/launch/avoi_dance.launch.py
+++ avoi_dance/launch/avoi_dance.launch.py
@@ -2,7 +2,7 @@ from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, TimerAction, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
-from launch.substitutions import LaunchConfiguration, ThisLaunchFileDir, PathJoinSubstitution
+from launch.substitutions import LaunchConfiguration
 from ament_index_python.packages import get_package_share_directory
 import os
 
@@ -10,83 +10,97 @@ def generate_launch_description():
     bringup_dir_avoi = os.path.join(get_package_share_directory('avoi_dance'), 'launch')
     bringup_dir = os.path.join(get_package_share_directory('irobot_create_gazebo_bringup'), 'launch')
 
-    # Launch for the first robot in Gazebo
-    robot_1_launch = IncludeLaunchDescription(
-    PythonLaunchDescriptionSource(os.path.join(bringup_dir_avoi, 'create3_gazebo_aws_small.launch.py')),
-    launch_arguments={'namespace': 'robot_1', 'x': '0.0'}.items(),
-    )
+    # Set the robots position and orientation
+    x_robot_1 = '0.0'
+    y_robot_1 = '0.0'
+    yaw_robot_1 = '0.0'
 
-    # Launch for the second robot with delay
-    robot_2_launch_delayed = TimerAction(
-    period=10.0,
-    actions=[
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(os.path.join(bringup_dir, 'create3_spawn.launch.py')),
-            launch_arguments={'namespace': 'robot_2', 'spawn_dock': 'false', 'x': '1.0'}.items(),
-            )
-        ]
-    )
-
-    # Declare launch arguments for nmespace, and odometry topic
-    declare_namespace_argument_robot_1 = DeclareLaunchArgument(
-        'namespace_1',
-        default_value='/robot_1',
-        description='Namespace for the robot 1 navigation node.')
-    
-    declare_odom_topic_argument_robot_1 = DeclareLaunchArgument(
-        'odom_topic_1',
-        default_value='/robot_1/odom',
-        description='Odometry topic for the robot 1.')
-        
-    declare_namespace_argument_robot_2 = DeclareLaunchArgument(
-        'namespace_2',
-        default_value='/robot_2',
-        description='Namespace for the robot 2 navigation node.')
-    
-    declare_odom_topic_argument_robot_2 = DeclareLaunchArgument(
-        'odom_topic_2',
-        default_value='/robot_2/odom',
-        description='Odometry topic for the robot 2.')
-
-    # Avoidance node for robot 1
-    robot_nav_node_1 = Node(
-        package='avoi_dance',
-        executable='robot_nav',
-        arguments=[
-            LaunchConfiguration('namespace_1'),
-            LaunchConfiguration('odom_topic_1'),
-        ],
-        output='screen',
-    )
-
-    # Avoidance node for robot 2
-    robot_nav_node_2 = Node(
-        package='avoi_dance',
-        executable='robot_nav',
-        arguments=[
-            LaunchConfiguration('namespace_2'),
-            LaunchConfiguration('odom_topic_2'),
-            ],
-        output='screen',
-    )
-
-    # Timer action to delay the launch of avoi_dance nodes
-    delay_robot_nav_node_1 = TimerAction(
-        period=30.0,
-        actions=[robot_nav_node_1],
-    )
-    delay_robot_nav_node_2 = TimerAction(
-        period=30.0,
-        actions=[robot_nav_node_2],
-    )
+    x_robot_2 = '2.0'
+    y_robot_2 = '0.0'
+    yaw_robot_2 = '0.0'
 
     return LaunchDescription([
-        declare_namespace_argument_robot_1,
-        declare_namespace_argument_robot_2,
-        declare_odom_topic_argument_robot_1,
-        declare_odom_topic_argument_robot_2,
-        robot_1_launch,
-        robot_2_launch_delayed,
-        delay_robot_nav_node_1,
-        delay_robot_nav_node_2,
+        # Launch for the first robot in Gazebo
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(os.path.join(bringup_dir_avoi, 'create3_gazebo_aws_small.launch.py')),
+            launch_arguments={'x': x_robot_1, 'y': y_robot_1, 'yaw': yaw_robot_1}.items(),
+        ),
+        
+        # Launch for the second robot with delay
+        TimerAction(
+            period=10.0,
+            actions=[
+                IncludeLaunchDescription(
+                    PythonLaunchDescriptionSource(os.path.join(bringup_dir, 'create3_spawn.launch.py')),
+                    launch_arguments={'namespace': 'robot_2', 'spawn_dock': 'false', 'x': x_robot_2, 'y': y_robot_2, 'yaw': yaw_robot_2}.items(),
+                )
+            ]
+        ),
+        
+        # Declare launch arguments
+        DeclareLaunchArgument('namespace_1', default_value='robot_1', description='Namespace for the robot 1 navigation node.'),
+        DeclareLaunchArgument('odom_topic_1', default_value='/robot_1/odom', description='Odometry topic for the robot 1.'),
+        DeclareLaunchArgument('hazard_topic_1', default_value='/robot_1/hazard_detection', description='Hazard topic for the robot 1.'),
+        DeclareLaunchArgument('cmd_vel_topic_1', default_value='/robot_1/cmd_vel', description='Command velocity topic for the robot 1.'),
+        DeclareLaunchArgument('initial_x_robot_1', default_value=x_robot_1, description='Initial x position of robot_1'),
+        DeclareLaunchArgument('initial_y_robot_1', default_value=y_robot_1, description='Initial y position of robot_1'),
+        DeclareLaunchArgument('initial_yaw_robot_1', default_value=yaw_robot_1, description='Initial yaw of robot_1'),
+        DeclareLaunchArgument('namespace_2', default_value='robot_2', description='Namespace for the robot 2 navigation node.'),
+        DeclareLaunchArgument('odom_topic_2', default_value='/robot_2/odom', description='Odometry topic for the robot 2.'),
+        DeclareLaunchArgument('hazard_topic_2', default_value='/robot_2/hazard_detection', description='Hazard topic for the robot 2.'),
+        DeclareLaunchArgument('cmd_vel_topic_2', default_value='/robot_2/cmd_vel', description='Command velocity topic for the robot 2.'),
+        DeclareLaunchArgument('initial_x_robot_2', default_value=x_robot_2, description='Initial x position of robot_2'),
+        DeclareLaunchArgument('initial_y_robot_2', default_value=y_robot_2, description='Initial y position of robot_2'),
+        DeclareLaunchArgument('initial_yaw_robot_2', default_value=yaw_robot_2, description='Initial yaw of robot_2'),
+        
+        # Timer action to delay the launch of avoi_dance nodes
+        TimerAction(
+            period=25.0,
+            actions=[
+                Node(
+                    package='avoi_dance',
+                    executable='robot_nav',
+                    namespace=LaunchConfiguration('namespace_1'),
+                    parameters=[{
+                        'namespace_': LaunchConfiguration('namespace_1'),
+                        'odometry_topic_self': LaunchConfiguration('odom_topic_1'),
+                        'odometry_topic_other': LaunchConfiguration('odom_topic_2'),
+                        'hazard_detection_topic': LaunchConfiguration('hazard_topic_1'),
+                        'cmd_vel_topic': LaunchConfiguration('cmd_vel_topic_1'),
+                        'initial_x_self': LaunchConfiguration('initial_x_robot_1'),
+                        'initial_y_self': LaunchConfiguration('initial_y_robot_1'),
+                        'initial_yaw_self': LaunchConfiguration('initial_yaw_robot_1'),
+                        'initial_x_other': LaunchConfiguration('initial_x_robot_2'),
+                        'initial_y_other': LaunchConfiguration('initial_y_robot_2'),
+                        'initial_yaw_other': LaunchConfiguration('initial_yaw_robot_2'),
+                    }],
+                    output='screen',
+                )
+            ]
+        ),
+        
+        TimerAction(
+            period=30.0,
+            actions=[
+                Node(
+                    package='avoi_dance',
+                    executable='robot_nav',
+                    namespace=LaunchConfiguration('namespace_2'),
+                    parameters=[{
+                        'namespace_': LaunchConfiguration('namespace_2'),
+                        'odometry_topic_self': LaunchConfiguration('odom_topic_2'),
+                        'odometry_topic_other': LaunchConfiguration('odom_topic_1'),
+                        'hazard_detection_topic': LaunchConfiguration('hazard_topic_2'),
+                        'cmd_vel_topic': LaunchConfiguration('cmd_vel_topic_2'),
+                        'initial_x_self': LaunchConfiguration('initial_x_robot_2'),
+                        'initial_y_self': LaunchConfiguration('initial_y_robot_2'),
+                        'initial_yaw_self': LaunchConfiguration('initial_yaw_robot_2'),
+                        'initial_x_other': LaunchConfiguration('initial_x_robot_1'),
+                        'initial_y_other': LaunchConfiguration('initial_y_robot_1'),
+                        'initial_yaw_other': LaunchConfiguration('initial_yaw_robot_1'),
+                    }],
+                    output='screen',
+                )
+            ]
+        ),
     ])
--- avoi_dance/src/robot_nav.cpp
+++ avoi_dance/src/robot_nav.cpp
@@ -1,42 +1,272 @@
 #include <rclcpp/rclcpp.hpp>
+#include <rclcpp_action/rclcpp_action.hpp>
+#include <irobot_create_msgs/action/rotate_angle.hpp>
 #include <nav_msgs/msg/odometry.hpp>
-#include <string>
+#include <irobot_create_msgs/msg/hazard_detection_vector.hpp>
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp> 
+#include <geometry_msgs/msg/twist.hpp>
+#include <cstdlib>
+#include <ctime>
+#include <memory>
 
-class RobotNav : public rclcpp::Node
-{
+#define M_PI 3.14159265358979323846
+
+/**
+ * @brief Enum class for the robot's behavior state
+ * 
+ */
+enum class BehaviorState {
+    IDLE,
+    ROTATING,
+    FORWARD
+};
+
+/**
+ * @brief Class for the autonomous navigation of the robot and avoidance of the other robot
+ * 
+ */
+class RobotNav : public rclcpp::Node {
 public:
-    RobotNav(const std::string& namespace_, const std::string& odometry_topic)
-    : Node("robot_nav", namespace_)
-    {
-        odometry_subscription_ = this->create_subscription<nav_msgs::msg::Odometry>(
-            odometry_topic, 10,
-            std::bind(&RobotNav::odometry_callback, this, std::placeholders::_1));
+    RobotNav() : Node("robot_nav") {
+
+        // Declare all necessary parameters
+        this->declare_parameter<std::string>("odometry_topic_self", "");
+        this->declare_parameter<std::string>("odometry_topic_other", "");
+        this->declare_parameter<std::string>("hazard_detection_topic", "");
+        this->declare_parameter<std::string>("cmd_vel_topic", "");
+        this->declare_parameter<double>("initial_x_self", 0.0);
+        this->declare_parameter<double>("initial_y_self", 0.0);
+        this->declare_parameter<double>("initial_yaw_self", 0.0);
+        this->declare_parameter<double>("initial_x_other", 0.0);
+        this->declare_parameter<double>("initial_y_other", 0.0);
+        this->declare_parameter<double>("initial_yaw_other", 0.0);
+
+        this->get_parameter("odometry_topic_self", odometry_topic_self);
+        this->get_parameter("odometry_topic_other", odometry_topic_other);
+        this->get_parameter("hazard_detection_topic", hazard_detection_topic);
+        this->get_parameter("cmd_vel_topic", cmd_vel_topic);
+        this->get_parameter("initial_x_self", initial_x_self);
+        this->get_parameter("initial_y_self", initial_y_self);
+        this->get_parameter("initial_yaw_self", initial_yaw_self);
+        this->get_parameter("initial_x_other", initial_x_other);
+        this->get_parameter("initial_y_other", initial_y_other);
+        this->get_parameter("initial_yaw_other", initial_yaw_other);
+
+        // Subscribe to the robot's odometry topic
+        odometry_subscription_self_ = this->create_subscription<nav_msgs::msg::Odometry>(
+            odometry_topic_self, 1,
+            std::bind(&RobotNav::odometry_callback_self, this, std::placeholders::_1));
+
+        // Subscrine to the other robot's odometry topic
+        odometry_subscription_other_ = this->create_subscription<nav_msgs::msg::Odometry>(
+            odometry_topic_other, 1,
+            std::bind(&RobotNav::odometry_callback_other, this, std::placeholders::_1));
+
+        // Subscribe to the robot's hazard detection topic
+        hazard_detection_subscription_ = this->create_subscription<irobot_create_msgs::msg::HazardDetectionVector>(
+            hazard_detection_topic, 1,
+            std::bind(&RobotNav::hazard_detection_callback, this, std::placeholders::_1));
+
+        // Publish the robot's velocity commands
+        cmd_vel_publisher_ = this->create_publisher<geometry_msgs::msg::Twist>(cmd_vel_topic, 1);
+
+        // Create a timer to publish the velocity commands
+        timer_ = this->create_wall_timer(
+            std::chrono::milliseconds(1000),
+            std::bind(&RobotNav::publish_velocity, this));
+
+        // Initialize gloabl variables
+        behavior_state = BehaviorState::FORWARD;
+        x_self_ = initial_x_self;
+        y_self_ = initial_y_self;
+        yaw_self_ = initial_yaw_self;
+
+        // Log the initial global positions and orientations
+        RCLCPP_INFO(
+            this->get_logger(), 
+            "Initial global position of robot_self: x = %f, y = %f, yaw = %f radians",
+            initial_x_self, initial_y_self, initial_yaw_self);
+
+        RCLCPP_INFO(
+            this->get_logger(),
+            "Initial global position of robot_other: x = %f, y = %f, yaw = %f radians",
+            initial_x_other, initial_y_other, initial_yaw_other);
     }
 
 private:
-    void odometry_callback(const nav_msgs::msg::Odometry::SharedPtr msg)
+    /**
+     * @brief Callback function for the robot's odometry topic
+     * @details The new odometry coordinates of "robot_self" are transformed in global coordinates
+     * 
+     * @param[in] msg 
+     */
+    void odometry_callback_self(const nav_msgs::msg::Odometry::SharedPtr msg)
     {
-        RCLCPP_INFO(this->get_logger(), "Received odometry: position x: '%.2f', y: '%.2f'",
-                    msg->pose.pose.position.x, msg->pose.pose.position.y);
+        yaw_self_old_ = yaw_self_;
+
+        x_self_ = initial_x_self + msg->pose.pose.position.x;
+        y_self_ = initial_y_self + msg->pose.pose.position.y;
+        yaw_self_ = quaternion_to_yaw(msg->pose.pose.orientation);
+        yaw_self_ = normalize_angle(yaw_self_);
+        
+
+        RCLCPP_INFO(
+            this->get_logger(),
+            "Global position of robot_self: x = %f, y = %f, yaw = %f radians",
+            x_self_, y_self_, yaw_self_);
     }
 
-    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odometry_subscription_;
+    /**
+     * @brief Callback function for the other robot's odometry topic
+     * @details The new odometry coordinates of "robot_other" are transformed in global coordinates
+     * 
+     * @param[in] msg 
+     */
+    void odometry_callback_other(const nav_msgs::msg::Odometry::SharedPtr msg)
+    {
+        x_other_ = initial_x_other + msg->pose.pose.position.x;
+        y_other_ = initial_y_other + msg->pose.pose.position.y;
+        yaw_other_ = quaternion_to_yaw(msg->pose.pose.orientation);
+        yaw_other_ = normalize_angle(yaw_other_);
+
+        RCLCPP_INFO(
+            this->get_logger(),
+            "Global position of robot_other: x = %f, y = %f, yaw = %f radians",
+            x_other_, y_other_, yaw_other_);
+
+
+        // // Check if the other robot is too close (FEATURE NOT WARKING YET)
+        // if (std::pow(std::pow(x_self_ - x_other_, 2) + std::pow(y_self_ - y_other_, 2), 0.5) < 1.0) {
+
+        //     if (last_action_was_rotation) {
+        //         yaw_goal = 0.0;
+        //         last_action_was_rotation = false;
+        //     }
+        //     else {
+        //         yaw_goal = M_PI;
+        //         last_action_was_rotation = true;
+        //     }
+
+        //     RCLCPP_INFO(
+        //         this->get_logger(),
+        //         "Robot_other is too close! The robot_self should rotate 180 degrees");
+        // }
+    }
+
+    /**
+     * @brief Callback function for the robot's hazard detection topic (NOT WORKING YET - FIX THE HAZARD DETECTION TOPIC)
+     * @details The robot's behavior is updated based on the detected hazards.
+     *          If a bump is detected, the robot should rotate. This function works only for one robot, however the second 
+     *          one is not working yet (namespace issue ?).
+     * 
+     * @param[in] msg 
+     */
+    void hazard_detection_callback(const irobot_create_msgs::msg::HazardDetectionVector::SharedPtr msg) {
+        for (const auto& detection : msg->detections) {
+            if (detection.type == irobot_create_msgs::msg::HazardDetection::BUMP) {
+
+                behavior_state = BehaviorState::ROTATING;
+
+                RCLCPP_INFO(this->get_logger(),
+                            "Bump detected! The robot_self should rotate 180 degrees.");
+                break;
+            }
+        }
+    }
+
+    /**
+     * @brief Publish the robot's velocity commands
+     * @details The robot's velocity is based on the desired robot's behaviour.
+     */
+    void publish_velocity()
+    {
+        double x_speed;
+        double yaw_speed;
+
+        // Robot is going forward
+        if (behavior_state == BehaviorState::FORWARD) {
+            x_speed = 1.0;
+            yaw_speed = 0.0;
+        }
+        // Robot is rotating
+        else if (behavior_state == BehaviorState::ROTATING) {
+            x_speed = 0.0;
+            yaw_speed = 1.0;
+            behavior_state = BehaviorState::FORWARD;
+        }
+        // Robot is idle
+        else {
+            x_speed = 0.0;
+            yaw_speed = 0.0;
+        }
+
+        RCLCPP_INFO(
+            this->get_logger(),
+            "x_speed = %f, yaw_speed = %f",
+            x_speed, yaw_speed);
+
+        // Publish the velocity commands
+        auto msg = geometry_msgs::msg::Twist();
+        msg.linear.x = x_speed;
+        msg.angular.z = yaw_speed;
+        cmd_vel_publisher_->publish(msg);
+    }
+
+    /**
+     * @brief Convert a quaternion to a yaw angle
+     * 
+     * @param[in] quat 
+     * @return double 
+     */
+    double quaternion_to_yaw(const geometry_msgs::msg::Quaternion& quat) {
+        tf2::Quaternion tf_quat;
+        tf2::fromMsg(quat, tf_quat);
+        double roll, pitch, yaw;
+        tf2::Matrix3x3(tf_quat).getRPY(roll, pitch, yaw);
+        return yaw;
+    }
+
+    /**
+     * @brief Normalize an angle to the range [0, 2*PI)
+     * 
+     * @param[in] angle 
+     * @return double 
+     */
+    double normalize_angle(double angle) {
+        while (angle < 0) angle += 2.0 * M_PI;
+        while (angle >= 2.0 * M_PI) angle -= 2.0 * M_PI;
+        return angle;
+    }
+
+    // Declare the subscribers, publishers and the timer
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odometry_subscription_self_;
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odometry_subscription_other_;
+    rclcpp::Subscription<irobot_create_msgs::msg::HazardDetectionVector>::SharedPtr hazard_detection_subscription_;
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_publisher_;
+    rclcpp::TimerBase::SharedPtr timer_;
+
+    // Retrieve the parameter values
+    std::string odometry_topic_self, odometry_topic_other, hazard_detection_topic, cmd_vel_topic;
+    double initial_x_self, initial_y_self, initial_yaw_self, initial_x_other, initial_y_other, initial_yaw_other;
+
+    // Declare the global variables
+    enum BehaviorState behavior_state;
+    double x_self_, y_self_, yaw_self_, yaw_self_old_; 
+    double x_other_, y_other_, yaw_other_;
 };
 
-int main(int argc, char** argv)
-{
-    if (argc < 3) {
-        std::cerr << "Usage: " << argv[0] << " <namespace> <odometry_topic>\n";
-        return 1;
-    }
-
-    std::string namespace_ = argv[1];
-    std::string odometry_topic = argv[2];
-
+/**
+ * @brief Main function
+ * 
+ * @param argc 
+ * @param argv 
+ * @return int 
+ */
+int main(int argc, char** argv) {
     rclcpp::init(argc, argv);
-    auto node = std::make_shared<RobotNav>(namespace_, odometry_topic);
+    auto node = std::make_shared<RobotNav>();
     rclcpp::spin(node);
     rclcpp::shutdown();
-
     return 0;
 }
